f9acc6245c1e9dece5f36e06fbe42525a49e0511
Alexander Vielimchanitsia <alexander_vielimchanitsia@mail.ua>
2015-04-06 23:54:05 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import render_template, flash, redirect
+
+from forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/question/add')
+def question_add():
+    return render_template('question_add.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form)
